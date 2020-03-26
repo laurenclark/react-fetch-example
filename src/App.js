05ce2e517b0b2282959d25b0db49c746c5ea352f
Lauren <lauren.c4231@gmail.com>
2020-03-26 00:12:26 +0000

    Refactor card

--- src/App.js
+++ src/App.js
@@ -23,27 +23,31 @@ function App() {
             <header>Fetch Users</header>
             <main>
                 {isLoading && <p>Loading...</p>}
-                {users.map(user => (
-                    <div className="card" key={user.id}>
-                        <p>
-                            <b>Name:</b> {nameFormat(user.username)}
-                        </p>
-                        <hr />
-                        <p>
-                            <b>Email:</b> {user.email}
-                        </p>
-                        <p>
-                            <b>Phone:</b> {user.phone}
-                        </p>
-                        <p>
-                            <b>Company:</b> {user.company.name} <br /> "
-                            <em>{user.company.catchPhrase}</em>"
-                        </p>
-                    </div>
-                ))}
+                {users.map(user => Card(user))}
             </main>
         </div>
     );
+
+    function Card(user) {
+        return (
+            <div className="card" key={user.id}>
+                <p>
+                    <b>Name:</b> {nameFormat(user.username)}
+                </p>
+                <hr />
+                <p>
+                    <b>Email:</b> {user.email}
+                </p>
+                <p>
+                    <b>Phone:</b> {user.phone}
+                </p>
+                <p>
+                    <b>Company:</b> {user.company.name} <br /> "
+                    <em>{user.company.catchPhrase}</em>"
+                </p>
+            </div>
+        );
+    }
 }
 
 export default App;
