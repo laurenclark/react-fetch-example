c2e613952ec3d43188f9871cfb5b9eff24911791
Lauren <lauren.c4231@gmail.com>
2020-04-09 21:40:03 +0100

    Add async/await to fetch call

--- src/components/fetch-users.js
+++ src/components/fetch-users.js
@@ -5,38 +5,61 @@ function FetchUsers() {
     const [users, setUsers] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [isError, setIsError] = useState(false);
-    const url = `https://jsonplaceholder.typicode.com/users`;
+
+    // useEffect(() => {
+    //     fetch(url, {
+    //         method: 'GET'
+    //     })
+    //         .then((data) => data.json())
+    //         .then((data) => {
+    //             setUsers(data);
+    //         })
+    //         .catch((err) => {
+    //             setIsError(true);
+    //             console.error(err);
+    //         })
+    //         .finally(setIsLoading(false));
+    // }, [url]);
 
     useEffect(() => {
-        fetch(url, {
-            method: 'GET'
-        })
-            .then((data) => data.json())
-            .then((data) => {
-                setUsers(data);
-            })
-            .catch((err) => {
-                setIsError(true);
-                console.error(err);
-            })
-            .finally(setIsLoading(false));
-    }, [url]);
+        let didCancel = false;
+        const url = `https://jsonplaceholder.typicode.com/users`;
+        const config = { method: 'GET' };
+        const fetchData = async () => {
+            if (!didCancel) {
+                try {
+                    const response = await fetch(url, config);
+                    const json = await response.json();
+                    setUsers(json);
+                } catch (error) {
+                    setIsError(true);
+                    console.error(error);
+                }
+                setIsLoading(false);
+            }
+        };
+        fetchData();
+        return () => {
+            didCancel = true;
+        };
+    }, []);
+
     return (
         <Fragment>
             {isLoading && <p>Loading...</p>}
             {isError && <div>Something went wrong...</div>}
-            {users
-                ? users.map((user) => (
-                      <Card
-                          key={user.id}
-                          username={user.name || 'n/a'}
-                          email={user.email || 'n/a'}
-                          phone={user.phone || 'n/a'}
-                          company={user.company.name || 'n/a'}
-                          catchPhrase={user.company.catchPhrase || 'n/a'}
-                      />
-                  ))
-                : 'No Results.'}
+            {users &&
+                users.map((user) => (
+                    <Card
+                        key={user.id}
+                        username={user.name || 'n/a'}
+                        email={user.email || 'n/a'}
+                        phone={user.phone || 'n/a'}
+                        company={user.company.name || 'n/a'}
+                        catchPhrase={user.company.catchPhrase || 'n/a'}
+                        test={user.poo || 'n/a'}
+                    />
+                ))}
         </Fragment>
     );
 }
