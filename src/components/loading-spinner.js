5e413ba08e92c23d618875a5db63c4e07a3543e1
Lauren <lauren.c4231@gmail.com>
2020-05-09 23:06:00 +0100

    Pass text props to load spinner

--- src/components/loading-spinner.js
+++ src/components/loading-spinner.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function LoadingSpinner() {
+function LoadingSpinner(props) {
     return (
         <div role="note" className="loading" aria-placeholder="Loading">
-            <h3>Loading...</h3>
+            <h3>{props.text}</h3>
             <div role="status" className="loading__ripple">
                 <div role="presentation"></div>
                 <div role="presentation"></div>
