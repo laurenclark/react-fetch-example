63117954003329325ceac71caa378101d4168535
Lauren <lauren.c4231@gmail.com>
2020-05-10 13:57:52 +0100

    Destructure props

--- src/components/loading-spinner.js
+++ src/components/loading-spinner.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function LoadingSpinner(props) {
+function LoadingSpinner({ text }) {
     return (
-        <div role="note" className="loading" aria-placeholder="Loading">
-            <h3>{props.text}</h3>
+        <div className="loading" aria-placeholder="Loading">
+            <h3>{text}</h3>
             <div role="status" className="loading__ripple">
                 <div role="presentation"></div>
                 <div role="presentation"></div>
